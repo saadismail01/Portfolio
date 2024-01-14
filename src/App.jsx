5261ea511c845aee7f766628238a4659409a26d4
Saad <saadismail001@gmail.com>
2024-01-14 14:02:59 +0200

    update in app project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Navbar from "./components/Navbar.tsx";
-import Home from "./components/Home.tsx";
-import About from "./components/About.tsx";
-import Projects from "./components/Projects.tsx";
-import Contacts from "./components/Contacts.tsx";
-import Footer from "./components/Footer.tsx";
+import Navbar from "./pages/Navbar.tsx";
+import Home from "./pages/Home.tsx";
+import About from "./pages/About.tsx";
+import Projects from "./pages/Projects.tsx";
+import Contacts from "./pages/Contacts.tsx";
+import Footer from "./pages/Footer.tsx";
 const App = () => {
   return (
     <div>
