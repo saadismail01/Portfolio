e1defe349b334444b287b4cfd85722d10ce2a224
Saad <saadismail001@gmail.com>
2024-01-17 22:59:45 +0200

    fixing on the logo pic

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
 import x from "./x.svg";
 import menu from "./menu.svg";
 import pic from "./pic.png";
+import logo from "./logo.png (2).png";
 
-export { x, menu, pic };
+export { x, menu, pic, logo };
