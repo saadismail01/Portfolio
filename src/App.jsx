d3fcc3a45eb67b5f856771b80c0ed5d621696c10
Saad <saadismail001@gmail.com>
2024-01-17 23:01:14 +0200

    add some styles and font for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
+/* eslint-disable react/no-unescaped-entities */
 import Navbar from "./pages/Navbar.tsx";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Projects from "./pages/Projects.tsx";
 import Contacts from "./pages/Contacts.tsx";
 import Footer from "./pages/Footer.tsx";
+
 const App = () => {
   return (
     <div>
-      <div className="w-full h-20">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
+      </style>
+      ;
+      <div className="w-full h-full">
         <Navbar />
       </div>
-      <div className="w-[100%] h-[100%]"></div>
-      <Home />
-      <About />
-      <Projects />
-      <Contacts />
-      <Footer />
+      <div className="w-full h-full">
+        <Home />
+      </div>
+      <div className="w-full h-full">
+        <About />
+      </div>
+      <div className="w-full h-full">
+        <Projects />
+      </div>
+      <div className="w-full h-full">
+        <Contacts />
+      </div>
+      <div className="w-full h-full">
+        <Footer />
+      </div>
     </div>
   );
 };
