e5a62e5410b0ef27621229494efce1e52ad959c3
Saad <saadismail001@gmail.com>
2024-01-29 13:40:02 +0200

    adding the animated typed style to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { pic } from "../../assets";
+import { TypeAnimation } from "react-type-animation";
 
 const Home = () => {
   return (
-    <div className="flex flex-col px-24 h-screen w-full items-center justify-center sm:flex sm:flex-row sm:justify-between sm:items-center">
-      <div className=" flex items-center justify-center ml-24 mt-28 md:mt-0">
+    <div className="flex flex-col h-screen w-full items-center justify-center sm:flex sm:flex-row sm:justify-between sm:items-center">
+      <div className=" flex items-center justify-center   mt-28 md:mt-0">
         <div className="flex flex-col space-y-12 ">
           <span className="text-teal-300 text-7xl fontText ">
             Hello..
@@ -14,10 +15,27 @@ const Home = () => {
           <h2 className="text-white text-4xl fontText">
             I'm Front End Developer
           </h2>
-          <h2 className="text-teal-300 text-3xl fontText">
-            It's more then Code,{" "}
-            <span className="text-white">It's passion...</span>
-          </h2>
+
+          <TypeAnimation
+            sequence={[
+              "It's more then Code, It's passion...", // Types 'One'
+              7000, // Waits 1s
+              // "", // Deletes 'One' and types 'Two'
+              // 2000, // Waits 2s
+              // "", // Types 'Three' without deleting 'Two'
+              () => {
+                console.log("Sequence completed");
+              },
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              color: "#5eead4",
+              fontSize: "2em",
+              display: "inline-block",
+            }}
+          />
         </div>
       </div>
 
