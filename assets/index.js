1129e2b66b629fa503d9de03d09c914a0a27c1c3
Saad <saadismail001@gmail.com>
2024-02-12 19:06:46 +0200

    upload video

--- assets/index.js
+++ assets/index.js
@@ -2,5 +2,5 @@ import x from "./x.svg";
 import menu from "./menu.svg";
 import pic from "./pic.png";
 import logo from "./logo.png (2).png";
-
-export { x, menu, pic, logo };
+import co from "./co.mp4";
+export { x, menu, pic, logo, co };
