309181ea39e599bef4ac62ad84365807611acb14
Saad <saadismail001@gmail.com>
2024-02-13 12:07:37 +0200

    add a videos and picture to the project

--- assets/index.js
+++ assets/index.js
@@ -3,4 +3,7 @@ import menu from "./menu.svg";
 import pic from "./pic.png";
 import logo from "./logo.png (2).png";
 import co from "./co.mp4";
-export { x, menu, pic, logo, co };
+import kime from "./kime.mp4";
+import bank from "./bank.mp4";
+import pexel from "./pexel.jpg";
+export { x, menu, pic, logo, co, kime, bank, pexel };
