1148736825b4e68776758bb8ec90a59e2e1e4e8c
Saad <saadismail001@gmail.com>
2024-02-14 22:13:26 +0200

    add the bg for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,33 +5,49 @@ import { TypeAnimation } from "react-type-animation";
 const Home = () => {
   return (
     <div className=" px-24 flex items-center justify-center h-screen w-full -mt-10 sm:flex sm:flex-row sm:justify-between sm:items-center">
-      <div className="flex justify-center items-center w-full h-full flex-col space-y-12 ">
-        <span className="text-teal-300 text-7xl fontText ">Hi..</span>
-        <span className="text-teal-300 text-6xl fontText">
-          This is <span className="text-white">Saad </span>Ismail.
-        </span>
-        <h2 className="text-white text-4xl fontText">a Front End Developer</h2>
+      <div className="area">
+        <div className="flex justify-center items-center w-full h-full flex-col space-y-12 ">
+          <span className="text-teal-300 text-7xl fontText ">Hi..</span>
+          <span className="text-teal-300 text-6xl fontText">
+            This is <span className="text-white">Saad </span>Ismail.
+          </span>
+          <h2 className="text-white text-4xl fontText">
+            a Front End Developer
+          </h2>
 
-        <TypeAnimation
-          sequence={[
-            "It's more then Code, ", // Types 'One'
-            1000, // Waits 1s
-            "It's passion...", // Deletes 'One' and types 'Two'
-            1000, // Waits 2s
-            // "ReactJS,TS,Angular...", // Types 'Three' without deleting 'Two'
-            () => {
-              console.log("Sequence completed");
-            },
-          ]}
-          wrapper="span"
-          cursor={true}
-          repeat={Infinity}
-          style={{
-            color: "#5eead4",
-            fontSize: "2em",
-            display: "inline-block",
-          }}
-        />
+          <TypeAnimation
+            sequence={[
+              "It's more then Code, ", // Types 'One'
+              1000, // Waits 1s
+              "It's passion...", // Deletes 'One' and types 'Two'
+              1000, // Waits 2s
+              // "ReactJS,TS,Angular...", // Types 'Three' without deleting 'Two'
+              () => {
+                console.log("Sequence completed");
+              },
+            ]}
+            wrapper="span"
+            cursor={true}
+            repeat={Infinity}
+            style={{
+              color: "#5eead4",
+              fontSize: "2em",
+              display: "inline-block",
+            }}
+          />
+        </div>
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
     </div>
   );
