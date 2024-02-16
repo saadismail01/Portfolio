7a8bc720a186333385c33b88418b91048e8e66cd
Saad <saadismail001@gmail.com>
2024-02-16 16:58:50 +0200

    Changed the text "Try it!" to "check it!" in the Navbar component.

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => {
               <span className="absolute w-36 h-32 -top-8 -left-2 bg-teal-300 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-bottom"></span>
               <span className="absolute w-36 h-32 -top-8 -left-2 bg-teal-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-bottom"></span>
               <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">
-                Try it!
+                Check it!
               </span>
             </button>
           </>
