8b637c356c97eb7a439235c5ea59151dfb731099
Saad <saadismail001@gmail.com>
2024-02-20 21:48:55 +0200

    add animation on contact page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Contacts = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
-    <div id="contacts" className="w-full h-screen mt-72 lg:mt-0">
+    <div id="contacts" className="w-full h-screen mt-72 lg:mt-34">
       <div className="  bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
         <h1 className="sm:text-5xl pb-16 w-full h-[20%] flex items-start justify-center text-teal-300 fontText lg:text-7xl mb-16">
           Contact Us!
         </h1>
-        <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div
+          data-aos="zoom-in"
+          className="relative py-3 sm:max-w-xl sm:mx-auto"
+        >
           <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-blue-500  transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl shadow-teal-300 shadow-2xl animate-pulse"></div>
           <div className="text-white relative px-8 py-16 bg-gray-900 shadow-lg sm:rounded-3xl sm:p-20">
             <div className="text-center flex flex-col space-y-6">
