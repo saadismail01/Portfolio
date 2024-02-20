1b8b81aa00052d4c588ed2f8d01ebf1c3ccfea9e
Saad <saadismail001@gmail.com>
2024-02-20 19:41:42 +0200

    fixing the responsive web design for contacts page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Contacts = () => {
   return (
-    <div id="contacts" className="w-full h-screen">
+    <div id="contacts" className="w-full h-screen mt-72 lg:mt-0">
       <div className="  bg-gray-900 py-6 flex flex-col justify-center sm:py-12">
         <h1 className="sm:text-5xl pb-16 w-full h-[20%] flex items-start justify-center text-teal-300 fontText lg:text-7xl mb-16">
           Contact Us!
@@ -11,11 +11,11 @@ const Contacts = () => {
           <div className="absolute inset-0 bg-gradient-to-r from-teal-300 to-blue-500  transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl shadow-teal-300 shadow-2xl animate-pulse"></div>
           <div className="text-white relative px-8 py-16 bg-gray-900 shadow-lg sm:rounded-3xl sm:p-20">
             <div className="text-center flex flex-col space-y-6">
-              <p className="text-white text-2xl">
+              <p className="text-white text-lg lg:text-2xl">
                 We are always open to new opportunities. Feel free to contact us
                 with any questions or comments you may have.Have questions?
               </p>
-              <p className="text-4xl">
+              <p className="text-xl lg:text-4xl">
                 <a href="mailto:saadismail001@gmail.com">
                   saadismail001@gmail.com
                 </a>
