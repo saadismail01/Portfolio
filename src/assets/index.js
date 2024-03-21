6c520d6918c998d4a594d33e41fbaf7b5bc22861
Saad <saadismail001@gmail.com>
2024-03-21 16:18:17 +0200

    make index.js file for the svg

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import ts from "./ts.svg";
+import angular from "./angular.svg";
+import html from "./html.svg";
+import css from "./css.svg";
+import git from "./git.svg";
+import github from "./github.svg";
+import js from "./js.svg";
+import nodejs from "./nodejs.svg";
+import react from "./react.svg";
+import tailwind from "./tailwind.svg";
+
+export { ts, angular, html, css, git, github, js, nodejs, react, tailwind };
