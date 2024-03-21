99cd18471bd787047133e6ba817691b18804d98a
Saad <saadismail001@gmail.com>
2024-03-21 20:31:31 +0200

    add animation to project header

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,7 +8,10 @@ const Projects = () => {
       id="projects"
       className="flex flex-col w-full h-full items-center justify-center px-24 mt-44 lg:mt-24"
     >
-      <div className="sm:text-5xl pb-16 w-full h-[20%] flex items-start justify-center text-teal-300 fontText lg:text-7xl">
+      <div
+        data-aos="fade-down"
+        className="sm:text-5xl pb-16 w-full h-[20%] flex items-start justify-center text-teal-300 fontText lg:text-7xl"
+      >
         Some Of My Project
       </div>
       <div className="flex flex-col space-y-4 items-center justify-center lg:flex-row lg:justify-center lg:items-center lg:space-x-4">
