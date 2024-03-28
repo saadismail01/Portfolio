3a0572b861eb56466ee01acbc79a4fe8fc5f0d26
Saad <saadismail001@gmail.com>
2024-03-28 14:43:47 +0200

    add animation to the skills comp

--- src/components/Skillsss.tsx
+++ src/components/Skillsss.tsx
@@ -16,54 +16,67 @@ const Skillsss = () => {
   return (
     <div className="grid grid-cols-2 gap-6  md:grid-cols-4  md:gap-24 gap-y-10">
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px] "
         src={html}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={css}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={tailwind}
       />
 
-      <img className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]" src={js} />
+      <img
+        data-aos="fade-up"
+        className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
+        src={js}
+      />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={ts}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={react}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={angular}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={nodejs}
         alt=""
       />
 
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={github}
         alt=""
       />
       <img
+        data-aos="fade-up"
         className="w-[60px] h-[60px] md:w-[70px] md:h-[70px]"
         src={git}
         alt=""
