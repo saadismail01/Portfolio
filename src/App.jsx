ac382f687bac11c8987d3c0c5ad9a8a911e76274
CapitalAcademy <saadismail001@gmail.com>
2025-04-21 01:54:47 +0300

    update on the bg

--- src/App.jsx
+++ src/App.jsx
@@ -15,26 +15,32 @@ const App = () => {
         url('https://fonts.googleapis.com/css2?family=Rubik:wght@500&display=swap');
       </style>
       ;
-      <div>
-        <Navbar />
-      </div>
-      <div id="home">
-        <Home />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="project">
-        <Projects />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="contact">
-        <Contacts />
-      </div>
-      <div>
-        <Footer />
+            <div>
+              <Navbar />
+            </div>
+            <div id="home">
+              <Home />
+            </div>
+            <div id="about">
+              <About />
+            </div>
+            <div id="project">
+              <Projects />
+            </div>
+            <div id="skills">
+              <Skills />
+            </div>
+            <div id="contact">
+              <Contacts />
+            </div>
+            <div>
+              <Footer />
+            </div>
+
+      <div className="inset-0 -z-10 overflow-hidden sm:overflow-hidden">
+        <div className="absolute w-[500px] h-[500px] bg-teal-800 rounded-full blur-2xl opacity-10 bottom-10 left-1/4 animate-pulse-light">      <Home /></div>
+      <div className="absolute w-[900px] h-[700px] bg-purple-900 rounded-full blur-3xl opacity-10 bottom-20  left-1/4 animate-pulse-light">      <Home /></div>
+    
       </div>
     </div>
   );
